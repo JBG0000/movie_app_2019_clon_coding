ff8c1361ea82cdedc89a8d938ce4210fd31236a6
JBG0000 <practice9090@naver.com>
2021-09-12 16:33:00 +0900

    #2.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -5,20 +5,42 @@ import React from "react";
 //   return <h1>I like Potato</h1>;
 // }
 
-//위 코드와 동일 작동
-function Food({fav}) {  //{}사용하면 props 안붙이고 그 요소중 특정한것만 빼내기 가능
-  console.log(fav);
-  return <h1>I like {fav}</h1>  //props 사용
+function Food({ name, picture }) {
+  //{}사용하면 props 안붙이고 그 요소중 특정한것만 빼내기 가능
+  return (
+    <>
+    {/* name, pricture props 사용 */}
+      <h2>I like {name}</h2>  
+      <img src={picture}></img> 
+    </>
+  );
 }
+
+const foodILike = [
+  {
+    //name과 image가 있는 object, foodILike는 배열
+    name: "kamja",
+    image:
+      "https://mediahub.seoul.go.kr/wp-content/uploads/2016/09/61a2981f41200ac8c513a3cbc0010efe.jpg",
+  },
+  {
+    name: "kamjajun",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGQOEuiXOth2d35Cen-K60VqVnzS0hZ4UEIw&usqp=CAU",
+  },
+  {
+    name: "sayingkamja",
+    image: "https://s3.peing.net/t/uploads/user/icon/13022962/f9d6a089.jpeg",
+  },
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello!!!!!</h1>
-      <Food fav="kimchi"></Food> {/* props */}
-      <Food fav="ramen"></Food>
-      <Food fav="samgiopsal"></Food>
-      <Food fav="JJukumi"></Food>
+      {foodILike.map((dish) => (
+        <Food name={dish.name} picture={dish.image} />
+      ))}{" "}
+      {/* dish는 object!!*/}
     </div>
   );
 }
