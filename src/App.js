efb05dbcefd4e3c91c4878aa5e0421f3b9c33bc4
JBG0000 <practice9090@naver.com>
2021-09-12 16:57:29 +0900

    #2.3 map Recap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import React from "react";
 
+const foodILike = [
+  {
+    //name과 image가 있는 object, foodILike는 배열
+    id:1,
+    name: "kamja",
+    image:
+      "https://mediahub.seoul.go.kr/wp-content/uploads/2016/09/61a2981f41200ac8c513a3cbc0010efe.jpg",
+  },
+  {
+    id:2,
+    name: "kamjajun",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGQOEuiXOth2d35Cen-K60VqVnzS0hZ4UEIw&usqp=CAU",
+  },
+  {
+    id:3,
+    name: "sayingkamja",
+    image: "https://s3.peing.net/t/uploads/user/icon/13022962/f9d6a089.jpeg",
+  },
+];
+
 // function Food(props) {
 //   console.log(props.fav);
 //   return <h1>I like Potato</h1>;
@@ -11,36 +32,23 @@ function Food({ name, picture }) {
     <>
     {/* name, pricture props 사용 */}
       <h2>I like {name}</h2>  
-      <img src={picture}></img> 
+      <img src={picture} alt={name}/>
     </>
   );
 }
 
-const foodILike = [
-  {
-    //name과 image가 있는 object, foodILike는 배열
-    name: "kamja",
-    image:
-      "https://mediahub.seoul.go.kr/wp-content/uploads/2016/09/61a2981f41200ac8c513a3cbc0010efe.jpg",
-  },
-  {
-    name: "kamjajun",
-    image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGQOEuiXOth2d35Cen-K60VqVnzS0hZ4UEIw&usqp=CAU",
-  },
-  {
-    name: "sayingkamja",
-    image: "https://s3.peing.net/t/uploads/user/icon/13022962/f9d6a089.jpeg",
-  },
-];
+// function renderFood(dish) {
+//   console.log(dish)
+//   return <Food name={dish.name} picture={dish.image} />
+// }
 
 function App() {
   return (
     <div>
-      {foodILike.map((dish) => (
-        <Food name={dish.name} picture={dish.image} />
-      ))}{" "}
-      {/* dish는 object!!*/}
+        {/* {console.log(foodILike.map(renderFood))} */}
+        {foodILike.map((dish) => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} />
+      ))}
     </div>
   );
 }
